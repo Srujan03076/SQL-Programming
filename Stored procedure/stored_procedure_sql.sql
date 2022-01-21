f691915b644eb5737ba3e7268229050896cec433
“Srujan03076” <srujanmahaja5@gmail.com>
2022-01-21 18:41:23 +0530

    [Srujan]Added: stored procedure

--- Stored procedure/stored_procedure_sql.sql
+++ Stored procedure/stored_procedure_sql.sql
@@ -8,3 +8,33 @@ END //
 
 DELIMITER ;
 CALL GetAllhiredCandidates
+
+DELIMITER ;
+CALL GetAllhiredCandidates;
+
+DELIMITER //
+CREATE PROCEDURE FullName(in r_id varchar(40))
+BEGIN
+	declare incorrectFormat condition for 1366;
+	declare exit handler for incorrectFormat
+	select 'please enter integer format for id' Message; 
+	SELECT getfullName(r_id);
+END //
+DELIMITER ;
+
+call FullName('5');
+DROP PROCEDURE FullName;
+
+
+
+DELIMITER //
+CREATE PROCEDURE Age(in r_id varchar(40))
+BEGIN
+	declare incorrectFormat condition for 1366;
+	declare exit handler for incorrectFormat
+	select 'please enter integer format for id' Message; 
+	SELECT fn_calculate_age(r_id);
+END //
+DELIMITER ;
+
+call Age(5);
