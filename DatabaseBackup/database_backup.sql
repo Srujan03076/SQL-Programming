a83287349c53b60a88356b841c8df3afe76337c1
“Srujan03076” <srujanmahaja5@gmail.com>
2022-01-27 10:10:08 +0530

    [Srujan]Added: Backup for database lmsdb

---
+++ DatabaseBackup/database_backup.sql
@@ -0,0 +1,17 @@
+/* 
+creating database backup 
+*/
+C:\Users\Admin>cd ..
+
+C:\Users>cd ..
+
+C:\>cd "Program Files"
+
+C:\Program Files>cd MySQL
+
+C:\Program Files\MySQL>cd "MySQL Server 8.0"
+
+C:\Program Files\MySQL\MySQL Server 8.0>cd bin
+
+C:\Program Files\MySQL\MySQL Server 8.0\bin>mysqldump -u root -p lmsdb > "C:\Users\Admin\Desktop\SQL CFP\DatabaseBackup\lmsdb_backup.sql"
+Enter password: **********
